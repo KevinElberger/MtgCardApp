997fef10d0b722101db6e00dedd75ed0df969f69
kevin elberger <kevelberger@gmail.com>
2016-07-23 23:45:00 -0700

    add progress bar loader and additional css changes

--- app/public/js/controllers.js
+++ app/public/js/controllers.js
@@ -2,15 +2,21 @@ angular.module('mtgCardApp.controllers', []).
 controller('mtgController', function($scope, mtgAPIservice) {
     $scope.cardList = [];
     $scope.card = "";
-    $scope.hidden = 'hidden';
+    $scope.hideBar = 'hidden'; // hides progress bar until search
+    $scope.hidden = 'hidden'; // hides results until search
 
     $scope.cardList.search = function() {
-        $scope.card = $scope.cardName;
+        $scope.hideBar = '';
+        $scope.bar = "width: 80%;";
+        $scope.card = $scope.cardName; // Obtain query param from input
         mtgAPIservice.getCards($scope.card).then(function(response) {
             // Obtain relevant data from response
             $scope.cardList = response.data.cards;
         });
-        $scope.well = 'well';
-        $scope.hidden = '';
+        setTimeout(function() {
+            $scope.hideBar = 'hidden';
+            $scope.well = 'well';
+            $scope.hidden = '';
+        }, 300);
     }
 });
