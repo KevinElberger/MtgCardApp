0fa5631f83c331ff158c5071e7c31492cce6a61d
kevin elberger <kevelberger@gmail.com>
2016-07-23 17:30:15 -0700

    Use Express for back end and restructure directory

---
+++ app/app.js
@@ -0,0 +1,39 @@
+/**
+ * Module dependencies
+ */
+
+var express = require('express'),
+    routes = require('./routes'),
+    http = require('http'),
+    path = require('path');
+
+var app = module.exports = express();
+app.engine('html', require('ejs').renderFile);
+
+/**
+ * Configuration
+ */
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+var env = process.env.NODE_ENV || 'development';
+
+/**
+ * Routes
+ */
+
+// serve index and view partials
+app.get('/', routes.index);
+
+/**
+ * Start Server
+ */
+
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
