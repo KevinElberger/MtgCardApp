0c2806ecceb0afdc6b6bbb014098e60fb31c7bb3
kevin elberger <kevelberger@gmail.com>
2016-07-25 14:33:26 -0700

    Implement deck building form layout and basic functionality

--- app/public/components/deck-builder/deck-builder.component.js
+++ app/public/components/deck-builder/deck-builder.component.js
@@ -1,7 +1,18 @@
 angular.module('deckBuilder').
     component('deckBuilder', {
         templateUrl: 'components/deck-builder/deck-builder.template.html',
-        controller: function DeckBuilderController() {
+        controller: function DeckBuilderController($scope, mtgAPIservice) {
+            $scope.cardList = [];
+            $scope.card = "";
+            $scope.hidden = 'hidden'; // Hide image until search is made
 
+            $scope.cardList.search = function() {
+                $scope.card = $scope.cardName; // Obtain query param from input
+                mtgAPIservice.getCards($scope.card).then(function(response) {
+                    // Obtain relevant data from response
+                    $scope.cardList = response.data.cards;
+                });
+                $scope.hidden = '';
+            }
         }
     });
