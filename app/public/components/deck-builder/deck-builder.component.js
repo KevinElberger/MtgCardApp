a1cb864ad92b1205b666cd2e078a4efc47d074a2
kevin elberger <kevelberger@gmail.com>
2016-07-25 15:10:52 -0700

    Implement card adding functionality for deck builder

--- app/public/components/deck-builder/deck-builder.component.js
+++ app/public/components/deck-builder/deck-builder.component.js
@@ -3,6 +3,9 @@ angular.module('deckBuilder').
         templateUrl: 'components/deck-builder/deck-builder.template.html',
         controller: function DeckBuilderController($scope, mtgAPIservice) {
             $scope.cardList = [];
+            $scope.mainBoard = ""; // Card names for main board
+            $scope.sideBoard = ""; // Card names for side board
+            $scope.deck = [];
             $scope.card = "";
             $scope.hidden = 'hidden'; // Hide image until search is made
 
@@ -13,6 +16,16 @@ angular.module('deckBuilder').
                     $scope.cardList = response.data.cards;
                 });
                 $scope.hidden = '';
+            }
+
+            $scope.deck.addCard = function(card) {
+                console.log($scope.board);
+                if($scope.board == true) {
+                    $scope.mainBoard += ($scope.quantity + "x " + $scope.cardName + "\n");
+                }
+                if($scope.board == false) {
+                    $scope.sideBoard += ($scope.quantity + "x " + $scope.cardName + "\n");
+                }
             }
         }
     });
