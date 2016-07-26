d4f7bb459f82854c8d956f8546c4af9c6e4784a1
kevin elberger <kevelberger@gmail.com>
2016-07-25 20:15:31 -0700

    Fix card image bug and implement deck saving in txt files

--- app/public/components/deck-builder/deck-builder.component.js
+++ app/public/components/deck-builder/deck-builder.component.js
@@ -8,16 +8,18 @@ angular.module('deckBuilder').
             $scope.deck = [];
             $scope.card = "";
             $scope.hidden = 'hidden'; // Hide image until search is made
+            var that = $scope;
 
-            $scope.cardList.search = function() {
+            $scope.search = function() {
                 $scope.card = $scope.cardName; // Obtain query param from input
                 mtgAPIservice.getCards($scope.card).then(function(response) {
                     // Obtain relevant data from response
-                    $scope.cardList = response.data.cards;
+                    that.cardList = response.data.cards;
                 });
                 $scope.hidden = '';
-            }
+            };
 
+            // Adds cards to either mainboard or sideboard
             $scope.deck.addCard = function(card) {
                 console.log($scope.board);
                 if($scope.board == true) {
@@ -26,6 +28,16 @@ angular.module('deckBuilder').
                 if($scope.board == false) {
                     $scope.sideBoard += ($scope.quantity + "x " + $scope.cardName + "\n");
                 }
-            }
+            };
+
+            // Saves deck as local text file
+            $scope.deck.saveDeck = function() {
+                var format = "Deck Format: " + $scope.deckFormat + "\r\n";
+                var main = "Mainboard:\r\n" + $scope.mainBoard + "\r\n";
+                var side = "Sideboard:\r\n" + $scope.sideBoard + "\r\n";
+                var notes = "Notes about deck:\r\n " + $scope.notes;
+                var deck = new Blob([format, main, side, notes], {type: "text/plain;charset=utf-8"});
+                saveAs(deck, "deck.txt");
+            };
         }
     });
--- app/public/js/services.js
+++ app/public/js/services.js
@@ -8,7 +8,6 @@ angular.module('mtgCardApp.services', []).
                 method: 'GET',
                 url: 'https://api.magicthegathering.io/v1/cards?name="' + name + '"'
             }).then(function successCallback(response) {
-                console.log(response.data.cards);
                 return response;
             }, function errorCallback(response) {
                 console.log(response);
