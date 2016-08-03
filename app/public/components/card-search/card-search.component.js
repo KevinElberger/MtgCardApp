9289c5bd96b91939416562893123b1811a04bf25
kevin elberger <kevelberger@gmail.com>
2016-08-03 11:09:18 -0700

    Add price fetching via echomtg API calls for card searches

--- app/public/components/card-search/card-search.component.js
+++ app/public/components/card-search/card-search.component.js
@@ -3,9 +3,11 @@ angular.module('cardSearch').
         templateUrl: 'components/card-search/card-search.template.html',
         controller:  function CardSearchController($scope, mtgAPIservice) {
             this.cardList = [];
-            this.card = "";
+            this.card = '';
             this.hideBar = 'hidden'; // hides progress bar until search
             this.hidden = 'hidden'; // hides results until search
+            this.token = '';
+            this.price = [0,0,0]; // high, low, mid
             var that = this;
 
             this.search = function(name) {
@@ -20,6 +22,17 @@ angular.module('cardSearch').
                     that.well = 'well';
                     that.hidden = '';
                 }, 300);
+
+                mtgAPIservice.getToken().then(function(response) {
+                    that.token = response;
+
+                    mtgAPIservice.getPrice(that.cardList[0].multiverseid, that.token).then(function(response) {
+                        that.price[0] = response.data.card.tcg_high;
+                        that.price[1] = response.data.card.tcg_low;
+                        that.price[2] = response.data.card.tcg_mid;
+                        console.log(that.price);
+                    });
+                });
             };
         }
     });
--- app/public/js/services.js
+++ app/public/js/services.js
@@ -13,5 +13,28 @@ angular.module('mtgCardApp.services', []).
                 console.log(response);
             });
         };
+
+        mtgAPI.getToken = function() {
+            return $http({
+                method: 'POST',
+                url: 'https://www.echomtg.com/api/user/auth/&email=kevelberger@gmail.com&password=whitehorse9'
+            }).then(function successCallback(response) {
+                return response.data.token;
+            }, function errorCallback(response) {
+                console.log(response);
+            });
+        };
+
+        mtgAPI.getPrice = function(multiverseID, token) {
+            return $http({
+                method: 'GET',
+                url: 'https://www.echomtg.com/api/inventory/add/mid=' + multiverseID + '&auth=' + token
+            }).then(function successCallback(response) {
+                return response;
+            }, function errorCallback(response) {
+                console.log(response);
+            });
+        };
+
         return mtgAPI;
 });
