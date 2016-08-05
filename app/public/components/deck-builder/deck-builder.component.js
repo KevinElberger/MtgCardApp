e409f15f67b831020df0aab163cc8816da64b040
kevin elberger <kevelberger@gmail.com>
2016-08-05 11:54:35 -0700

    Refactor deck builder component

--- app/public/components/deck-builder/deck-builder.component.js
+++ app/public/components/deck-builder/deck-builder.component.js
@@ -2,83 +2,66 @@ angular.module('deckBuilder').
     component('deckBuilder', {
         templateUrl: 'components/deck-builder/deck-builder.template.html',
         controller: function DeckBuilderController($scope, mtgAPIservice) {
-            this.card = "";
             this.hidden = 'hidden'; // Hide image until search is made
-            this.cardStats = [];
-            this.cardTypes = [0,0,0,0,0,0,0];
+            this.cardStats = []; // Collection of cards
+            this.cardTypes = [0,0,0,0,0,0,0]; // Creature, sorcery, instant, artifact, enchantment, planeswalker, land
             this.cardColors = [{color: "White", count: 0}, {color: "Black", count: 0}, {color: "Blue", count: 0}, {color: "Red", count: 0}, {color: "Green", count: 0}];
             this.cardCount = 0;
-            this.average = 0;
-            this.sum = 0;
-            this.row1 = [], this.row2 = [], this.row3 = [], this.row4 = [];
             this.cmc = [0,0,0,0,0,0,0,0];
             var that = this;
 
-            // Adds cards to either mainboard or sideboard
-            // and stores cards for statistics
+            // Adds cards for statistics and display purposes
+            // @param card (String) - card name
             this.addCard = function(card) {
-                // if (quantity > 0) {
-                //     that.cardCount += quantity;
-
                 // Get card from API
                 mtgAPIservice.getCards(card).then(function (response) {
                     var data = response.data.cards[response.data.cards.length - 1];
                     var cardType = response.data.cards[response.data.cards.length - 1].types[0];
-
-                    if (that.cardCount < 15) {
-                        that.row1.push(response.data.cards[response.data.cards.length-1]);
-                    } else if (that.cardCount >= 15 && that.cardCount < 30) {
-                        that.row2.push(response.data.cards[response.data.cards.length-1]);
-                    } else if (that.cardCount >= 30 && that.cardCount < 45) {
-                        that.row3.push(response.data.cards[response.data.cards.length-1]);
-                    } else if (that.cardCount >= 45 && that.cardCount < 60) {
-                        that.row4.push(response.data.cards[response.data.cards.length-1]);
-                    }
                     that.cardCount++;
 
-                    // Obtain last card (most recent) from response and push into card stats array
-                    that.cardStats.push(response.data.cards[response.data.cards.length - 1]);
+                    // Obtain last card (most recent) from response and push into card collection
+                    that.cardStats.push(data);
                     // Keep track of cmc and push in appropriate array slot
-                    if (that.cardStats[that.cardStats.length - 1].cmc) {
-                        that.sum += that.cardStats[that.cardStats.length - 1].cmc;
+                    if (data.cmc) {
                         for (var j = 0; j < 8; j++) {
-                            if (j == that.cardStats[that.cardStats.length - 1].cmc) {
+                            if (j == data.cmc) {
                                 that.cmc[j] ++;
                             }
                         }
-                        if (that.cardStats[that.cardStats.length - 1].cmc > 7) {
+                        if (data.cmc > 7) {
                             that.cmc[7] ++;
                         }
                     }
-                    // Record color and quantity for polar chart
+                    // Record color for polar chart
                     that.recordColor(data);
-
                     switch (cardType) {
-                        case "Creature":
-                            that.cardTypes[0]++;
-                            break;
-                        case "Sorcery":
-                            that.cardTypes[1]++;
-                            break;
-                        case "Instant":
-                            that.cardTypes[2]++;
-                            break;
-                        case "Artifact":
-                            that.cardTypes[3]++;
-                            break;
-                        case "Enchantment":
-                            that.cardTypes[4]++;
-                            break;
-                        case "Planeswalker":
-                            that.cardTypes[5]++;
-                            break;
-                        default:
-                            // Lands
-                            that.cardTypes[6]++;
-                            break;
+                    case "Creature":
+                        that.cardTypes[0]++;
+                        break;
+                    case "Sorcery":
+                        that.cardTypes[1]++;
+                        break;
+                    case "Instant":
+                        that.cardTypes[2]++;
+                        break;
+                    case "Artifact":
+                        that.cardTypes[3]++;
+                        break;
+                    case "Enchantment":
+                        that.cardTypes[4]++;
+                        break;
+                    case "Planeswalker":
+                        that.cardTypes[5]++;
+                        break;
+                    default:
+                        // Lands
+                        that.cardTypes[6]++;
+                        break;
                     }
+                    var deck = document.getElementById('deck');
+                    deck.classList.remove('hidden');
                     // Update statistics graphs
-                    that.displayStats();
+                    that.computeStats();
                 });
             };
 
@@ -106,119 +89,120 @@ angular.module('deckBuilder').
                 this.textDeck = deck;
             };
 
-            this.displayStats = function() {
-                // Data for bar chart
-                var clicked = 0;
-                if (clicked < 2) {
-                    clicked++;
-                    this.computeStats();
-                }
-            };
-
             this.computeStats = function() {
-                that.average = that.sum / that.cardCount;
                 var statWrap = document.getElementById("statisticsWrapper");
                 statWrap.classList.remove("hidden");
                 $('.canvasWrapper').empty().append('<label>Mana Curve</label><canvas id="bar" width="250" height="250"></canvas>' +
                 '<label>Type Breakdown</label><canvas id="pie" width="250" height="250"></canvas><br/>' + '<label>Color Breakdown</label>' +
                 '<canvas id="polar" width="300" height="300"></canvas>');
+
+                // Create charts
                 var ctx = document.getElementById('bar').getContext('2d');
-                var myBarChart = new Chart(ctx, {
-                    type: 'bar',
-                    data: {
-                        labels: ['0','1','2','3','4','5','6','7+'],
-                        datasets: [
-                            {
-                                label: '# of Cards',
-                                data: that.cmc,
-                                backgroundColor: ['#FF8B66',
-                                    '#5B62BC',
-                                    '#FFD666',
-                                    '#DF5988',
-                                    '#4C8BB0',
-                                    '#4CBE84',
-                                    '#FFBA66'
-                                ],
-                                borderColor: ['#FF8B66',
-                                    '#5B62BC',
-                                    '#FFD666',
-                                    '#DF5988',
-                                    '#4C8BB0',
-                                    '#4CBE84',
-                                    '#FFBA66'
-                                ]
-                            }
-                        ],
-                        options: {
-                            responsiveAnimationDuration: 1000,
-                            maintainAspectRatio: false
-                        }
-                    }
-                });
-
                 var pie = document.getElementById('pie').getContext('2d');
-                var myPieChart = new Chart(pie, {
-                    type: 'pie',
-                    data: {
-                        labels: [
-                            "Creature",
-                            "Sorcery",
-                            "Instant",
-                            "Artifact",
-                            "Enchantment",
-                            "Planeswalker",
-                            "Land"
-                        ],
-                        datasets: [
-                            {
-                                data: [that.cardTypes[0], that.cardTypes[1], that.cardTypes[2], that.cardTypes[3],
-                                    that.cardTypes[4], that.cardTypes[5], that.cardTypes[6]],
-                                backgroundColor: [
-                                    "#4CBE84",
-                                    "#FF8B66",
-                                    "#4C8BB0",
-                                    "#FFD666",
-                                    "#DF5988",
-                                    "#4C8BB0",
-                                    "#FFBA66"
-                                ]
-                            }
-                        ],
-                        options: {
-                            responsiveAnimationDuration: 1000,
-                            maintainAspectRatio: false
-                        }
-                    }
-                });
+                var doughnut = document.getElementById('polar').getContext('2d');
+                that.createChart(ctx);
+                that.createChart(pie);
+                that.createChart(doughnut);
+            };
 
-                var polar = document.getElementById('polar').getContext('2d');
-                var myPolarChart = new Chart(polar, {
-                    type: 'doughnut',
-                    data: {
-                        labels: [
-                            "White",
-                            "Black",
-                            "Blue",
-                            "Red",
-                            "Green"
-                        ],
-                        datasets: [{
-                            data: [that.cardColors[0].count,that.cardColors[1].count,that.cardColors[2].count,
-                                that.cardColors[3].count,that.cardColors[4].count],
-                            backgroundColor: [
+            this.createChart = function(chart) {
+                console.log(chart.canvas.id);
+                if (chart.canvas.id == "polar") {
+                    var doughnutChart = new Chart(chart, {
+                        type: 'doughnut',
+                        data: {
+                            labels: [
+                                "White",
+                                "Black",
+                                "Blue",
+                                "Red",
+                                "Green"
+                            ],
+                            datasets: [{
+                                data: [that.cardColors[0].count,that.cardColors[1].count,that.cardColors[2].count,
+                                    that.cardColors[3].count,that.cardColors[4].count],
+                                backgroundColor: [
                                     "#FFD666",
                                     "#333333",
                                     "#4C8BB0",
                                     "#FF8B66",
                                     "#4CBE84"
-                            ]
-                        }],
-                        options: {
-                            responsiveAnimationDuration: 1000,
-                            maintainAspectRatio: false
+                                ]
+                            }],
+                            options: {
+                                responsiveAnimationDuration: 1000,
+                                maintainAspectRatio: false
+                            }
                         }
-                    }
-                });
+                    });
+                } else if (chart.canvas.id == "pie") {
+                    var pieChart = new Chart(chart, {
+                        type: 'pie',
+                        data: {
+                            labels: [
+                                "Creature",
+                                "Sorcery",
+                                "Instant",
+                                "Artifact",
+                                "Enchantment",
+                                "Planeswalker",
+                                "Land"
+                            ],
+                            datasets: [
+                                {
+                                    data: [that.cardTypes[0], that.cardTypes[1], that.cardTypes[2], that.cardTypes[3],
+                                        that.cardTypes[4], that.cardTypes[5], that.cardTypes[6]],
+                                    backgroundColor: [
+                                        "#4CBE84",
+                                        "#FF8B66",
+                                        "#4C8BB0",
+                                        "#FFD666",
+                                        "#DF5988",
+                                        "#4C8BB0",
+                                        "#FFBA66"
+                                    ]
+                                }
+                            ],
+                            options: {
+                                responsiveAnimationDuration: 1000,
+                                maintainAspectRatio: false
+                            }
+                        }
+                    });
+                } else if (chart.canvas.id == "bar") {
+                    var barChart = new Chart(chart, {
+                        type: 'bar',
+                        data: {
+                            labels: ['0','1','2','3','4','5','6','7+'],
+                            datasets: [
+                                {
+                                    label: '# of Cards',
+                                    data: that.cmc,
+                                    backgroundColor: ['#333333',
+                                        '#4C8BB0',
+                                        '#FFD666',
+                                        '#FF8B66',
+                                        '#4C8BB0',
+                                        '#4CBE84',
+                                        '#FFBA66'
+                                    ],
+                                    borderColor: ['#333333',
+                                        '#4C8BB0',
+                                        '#FFD666',
+                                        '#FF8B66',
+                                        '#4C8BB0',
+                                        '#4CBE84',
+                                        '#FFBA66'
+                                    ]
+                                }
+                            ],
+                            options: {
+                                responsiveAnimationDuration: 1000,
+                                maintainAspectRatio: false
+                            }
+                        }
+                    });
+                }
             };
         }
     });
