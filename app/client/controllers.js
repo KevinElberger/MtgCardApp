776a43995d3d38ddc9aa44671df21c3ca0e2c6e5
kevin elberger <kevelberger@gmail.com>
2016-08-09 12:25:39 -0700

    Mostly finish landing page, re-do nav bar, fix small bugs

--- app/client/controllers.js
+++ app/client/controllers.js
@@ -12,7 +12,7 @@ angular.module('myApp').controller('loginController',
       AuthService.login($scope.loginForm.username, $scope.loginForm.password)
         // handle success
         .then(function () {
-          $location.path('/');
+          $location.path('/deckbuilder');
           $scope.disabled = false;
           $scope.loginForm = {};
         })
