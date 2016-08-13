feb7edf16d120ac436ad11e2bef9b48bf24111b7
kevin elberger <kevelberger@gmail.com>
2016-08-13 15:31:04 -0700

    Add right-click directive for deckbuilder component

--- app/client/components/deck-builder/deck-builder.module.js
+++ app/client/components/deck-builder/deck-builder.module.js
@@ -1,5 +1,17 @@
-angular.module('deckBuilder', [
+var app = angular.module('deckBuilder', [
     'ngRoute',
     'chart.js',
     'myApp.services'
 ]);
+
+app.directive('ngRightClick', function($parse) {
+    return function(scope, element, attrs) {
+        var fn = $parse(attrs.ngRightClick);
+        element.bind('contextmenu', function(event) {
+            scope.$apply(function() {
+                event.preventDefault();
+                fn(scope, {$event:event});
+            });
+        });
+    };
+});
