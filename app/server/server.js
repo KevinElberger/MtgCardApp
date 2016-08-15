7e17728001bb755d839b04dc0456284d88770e08
kevin elberger <kevelberger@gmail.com>
2016-08-15 12:36:37 -0700

    procfile/app.js changes

--- app/server/server.js
+++ app/server/server.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 // var debug = require('debug')('passport-mongo');
-var app = require('/app');
+var app = require('app');
 
 // app.set('port', 27017);
 
@@ -9,6 +9,6 @@ var app = require('/app');
 //   debug('Express server listening on port ' + server.address().port);
 // });
 
-var server = app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+// var server = app.listen(process.env.PORT || 3000, function(){
+//   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// });
