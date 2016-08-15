0df60fdb450206544b59f3baf715669696f5d617
kevin elberger <kevelberger@gmail.com>
2016-08-14 22:25:40 -0700

    Add backgrounds and random background display for login

--- app/client/main.js
+++ app/client/main.js
@@ -57,3 +57,13 @@ myApp.run(function ($rootScope, $location, $route, AuthService) {
       });
   });
 });
+
+myApp.directive('background', function() {
+  setInterval(function() {
+    $(".login-header").fadeTo('slow', 0.5, function() {
+      var curId = Math.round(Math.random() * 9) + 1;
+      $(".login-header").css("background-image", "url(../img/bg" + curId + ".jpg)");
+    }).fadeTo('fast','1');
+  }, 12000);
+  return {};
+});
