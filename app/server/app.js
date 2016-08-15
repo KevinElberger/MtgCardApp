8a65a3332cdd33468575c2518742c6e631b54132
kevin elberger <kevelberger@gmail.com>
2016-08-15 13:14:35 -0700

    express ports

--- app/server/app.js
+++ app/server/app.js
@@ -66,6 +66,7 @@ app.use(function(err, req, res) {
 
 module.exports = app;
 
-var server = app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+    console.log("Listening on: " + port);
 });
